cc8a18adce97d049cb22e24da5066bb4adb65eca
Milenio X <jnkmiloq@gmail.com>
2017-12-21 05:11:46 -0500

    Implementa Controlador para productos y ajusta las rutas de acceso

---
+++ simple-crud/app/controllers/ProductController.java
@@ -0,0 +1,34 @@
+package controllers;
+
+import play.mvc.*;
+
+public class ProductController extends Controller {
+
+    /**
+     * List all products
+     */
+    public Result list() {
+        return TODO;
+    }
+
+    /**
+     * Show a blank product form
+     */
+    public Result newProduct() {
+        return TODO;
+    }
+
+    /**
+     * Show a product edit form
+     */
+    public Result details(String ean) {
+        return TODO;
+    }
+
+    /**
+     * Save a product
+     */
+    public Result save() {
+        return TODO;
+    }
+}
